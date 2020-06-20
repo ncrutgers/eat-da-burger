557efed983c571c47c9a014a272132e049fbbcde
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-20 00:40:18 -0400

    Created db connection

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+var mysql =  require("mysql");
+
+var connection = mysql.createConnection({
+    host: "",
+    port: ,
+    user: "",
+    password: "",
+    database: "burgers_db"
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("Error connecting: " + err.stack);
+        return;
+    }
+    console.log("Connected as id " + connection.threadId);
+});
+
+module.exports = connection;
