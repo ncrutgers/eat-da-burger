7f26ad84b60166dd73546eafb0b048f1417fec0f
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-21 15:19:23 -0400

    Connecting remote db

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
 var mysql =  require("mysql");
-// Connect to mysql db w/ credentials
-var connection = mysql.createConnection({
-    host: "",
-    port: 3306,
-    user: "",
-    password: "",
-    database: "burgers_db"
+var connection;
+// If the server contains the JAWSDB_URL environmental variable, it	connect to the JawsDB database
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Connect to mysql locally db w/ credentials. If the server lacks the variable, it falls back on an explicitly defined local database.
+    connection = mysql.createConnection({
+        host: "",
+        user: "",
+        password: "",
+        database: "burgers_db"
 });
+
+}
+
 // Connects to mysql connection
 connection.connect(function(err) {
     if (err) {
