5d7676519d46b6aa7c61afca9aa0498e9f2801fb
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-21 04:23:31 -0400

    Comments added

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ var PORT = process.env.PORT || 3000;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory to the app directory
 app.use(express.static("public"));
 
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
+// Set Handlebars, engines main layout, and view engine
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
+// Import routes and give the server access to them
 var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
 app.listen(PORT, function() {
-  console.log("App now listening at localhost:" + PORT);
+  console.log("App now listening at http://localhost:" + PORT);
 });
