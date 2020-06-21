d9a587ca44b3ddfe3ce537236744011520080b52
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-06-21 04:22:03 -0400

    Comments added

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,13 @@
 var mysql =  require("mysql");
-
+// Connect to mysql db w/ credentials
 var connection = mysql.createConnection({
     host: "",
-    port: ,
+    port: 3306,
     user: "",
     password: "",
     database: "burgers_db"
 });
-
+// Connects to mysql connection
 connection.connect(function(err) {
     if (err) {
         console.log("Error connecting: " + err.stack);
